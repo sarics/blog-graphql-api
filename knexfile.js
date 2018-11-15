90668a836c844407a12e85ac74fc02c4a4bcfc49
sarics <sari.csaba@outlook.hu>
2018-11-15 20:14:57 +0100

    prepare for heroku

--- knexfile.js
+++ knexfile.js
@@ -2,22 +2,21 @@ require('dotenv').config();
 
 const config = {
   client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  },
   migrations: {
     directory: './database/migrations',
-    tableName: 'knex_migrations',
   },
 };
 
 module.exports = {
   development: {
     ...config,
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
     debug: true,
     searchPath: ['knex'],
     seeds: {
@@ -27,9 +26,9 @@ module.exports = {
 
   production: {
     ...config,
-    pool: {
-      min: 2,
-      max: 10,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
     },
   },
 };
